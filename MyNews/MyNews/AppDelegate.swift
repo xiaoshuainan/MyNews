ec6085fcc0219e8729138eba7c80de240e4fe8b1
效帅楠 <815758971@qq.com>
2018-12-26 18:48:07 +0800

    no message

--- MyNews/MyNews/AppDelegate.swift
+++ MyNews/MyNews/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        /// 设置主题的颜色
+        /// 设置主题颜色
         ThemeManager.setTheme(plistName: UserDefaults.standard.bool(forKey: isNight) ? "night_theme" : "default_theme", path: .mainBundle)
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = MyTabBarViewController()
